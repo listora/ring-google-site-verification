6bf355287b114fc9f4a3988fb9f1a875c4e322f4
James Conroy-Finn <james@logi.cl>
2014-10-29 12:44:48 +0000

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject listora/ring-google-site-verification "0.1.0-SNAPSHOT"
+(defproject listora/ring-google-site-verification "0.1.0"
   :description "Ring middleware for returning site verification files"
   :url "https://github.com/listora/ring-google-site-verification"
   :license {:name "The MIT License"
